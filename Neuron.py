aeea3864a56f637ffc8ceb3b6f9103861f0a2196
Eric Chen <eric7chen@yahoo.com>
2020-06-08 16:47:50 -0500

    crazy epic shit and put everything in one file for now

--- Neuron.py
+++ Neuron.py
@@ -1,20 +1,70 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 from tkinter.colorchooser import *
-from main import *
 
-COORDS = 50, 50, 100, 100
+global canvas
+global root
+global menu
+
+root = Tk()
+menu = Menu(root)
+canvas = Canvas(root, height=500, width=500)
+canvas.pack()
+
+coords = [50, 50, 100, 100]
+
 class Neuron:
     # default constructor
     def __init__(self):
-        canvas.create_oval(COORDS)
+        self.node = canvas.create_oval(coords, fill = 'black')
+        canvas.tag_bind(self.node, '<Button-3>', self.circle_click)
 
 
     def set_background(self, event = None):
         color = askcolor()
-        canvas.itemconfig(self.node, fill = color)
+        canvas.itemconfig(self.node, fill = color[1])
     
 
     def circle_click(self, event = None):
-        set_background(self)
+        self.set_background()
+    
 
+class Layer:
+    def __init__(self):
+        self.layer = [Neuron()]
+        self.next_location()
+
+
+    def set_color(self, color):
+        for i in self.layer:
+            i.set_background(color)
+    
+
+    def add_neuron(self):
+        self.layer.append(Neuron())
+    
+    def next_location(self):
+        coords[0] = coords[2]
+        coords[1] = coords[3]
+        coords[2] = coords[0] + 50
+        coords[3] = coords[1] + 50
+        print('maybe')
+    
+
+class NeuralNetwork:
+    def __init__(self):
+        self.network = [Layer()]
+
+    
+    def add_layer(self):
+        self.network.append(Layer())
+        canvas.update()
+        print('add_layer')
+
+    def get_network(self):
+        return self.network
+
+
+app = NeuralNetwork()
+test = Button(text = 'add', command = app.add_layer).pack()
+root.mainloop()
