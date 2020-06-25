876eab35a2b843192d27b586d6f57d8578ffb192
hughesbenm <hughesbenm@gmail.com>
2020-06-25 14:45:12 -0500

    Add files via upload

---
+++ Ben's GAN with Dur and Hour.py
@@ -0,0 +1,317 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pds
+import keras
+from keras.callbacks import TensorBoard
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.layers import Flatten
+from keras.layers import Reshape
+from keras.layers import UpSampling2D
+from keras.layers import UpSampling3D
+from keras.layers import BatchNormalization
+from keras.layers import Conv2DTranspose
+from keras.layers import Activation
+from keras.layers import Dropout
+from keras.layers import Conv2D
+from keras.layers.advanced_activations import LeakyReLU
+
+
+TRAIN_ROWS = 20000
+
+PREDICT_ROWS = 80
+
+
+# In[ ]:
+
+
+gen = Sequential()
+dis = Sequential()
+GAN = Sequential()
+
+
+# In[ ]:
+
+
+#This is Differences
+x_train = pds.read_excel(r'C:\Users\Ben\Desktop\Data\DifferencesData.xlsx', usecols=[17, 29], nrows=TRAIN_ROWS)
+
+#This is Exact Values
+# x_train = pds.read_excel(r'C:\Users\Ben\Desktop\Data\DifferencesData.xlsx', usecols=[1, 2, 4, 5, 17, 18, 19, 20, 21, 22], nrows=20000)
+x_train.head()
+
+
+# In[ ]:
+
+
+# plt.hist(x_train['START_HOUR'], density=True, bins=50)
+# plt.ylabel('Freq')
+
+# x_hist = ["" for x in range(20000)]
+
+# for i in range(20000):
+#     if x_train['DIFFERENCE'][i] == 0:
+#             x_hist[i] = "0"
+#     for j in range(404):
+#         if x_train['DIFFERENCE'][i] > j*60 and x_train['DIFFERENCE'][i] <= (j+1)*60:
+#             x_hist[i] = (j+1)
+
+# plt.hist(x_hist, density=True, bins=50)
+# plt.ylabel('Freq')
+
+# max = 1
+# for i in range(20000):
+#     if x_train['DIFFERENCE'][i] > max:
+#         max = x_train['DIFFERENCE'][i]
+# print(max)
+
+
+# In[ ]:
+
+
+y_train = pds.read_excel(r'C:\Users\Ben\Desktop\Data\DifferencesData.xlsx', usecols=[0], nrows=20000)
+y_train.head()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# x_predict = pds.read_excel(r'C:\Users\Ben\Desktop\Data\DifferencesData.xlsx', usecols=[17, 28], nrows=PREDICT_ROWS)
+# x_predict.head()
+# x_predict = np.ones((1, 10))
+# x_predict[0] = [-84.32392, -84.32392, 34.0364, 34.0364, 0, 0, 5, 21, 56, 7]
+
+
+# In[ ]:
+
+
+#Differences
+x_test = pds.read_excel(r'C:\Users\Ben\Desktop\Data\DifferencesData.xlsx', skiprows=220000, usecols=[17, 28], nrows=2500)
+
+#Exact
+# x_test = pds.read_excel(r'C:\Users\Ben\Desktop\Data\DifferencesData.xlsx', skiprows=220000, usecols=[1, 2, 4, 5, 17, 18, 19, 20, 21, 22], nrows=2500)
+x_test.head()
+
+
+# In[ ]:
+
+
+y_test = pds.read_excel(r'C:\Users\Ben\Desktop\Data\DifferencesData.xlsx', usecols=[0], skiprows=220000, nrows=2500)
+y_test.head()
+
+
+# In[ ]:
+
+
+gen.add(Dense(75, input_shape=(200,), activation='relu'))
+gen.add(BatchNormalization(momentum=0.9))
+gen.add(Dropout(0.4))
+gen.add(Dense(50, activation='relu'))
+gen.add(BatchNormalization(momentum=0.9))
+gen.add(Dense(25, activation='relu'))
+gen.add(BatchNormalization(momentum=0.9))
+gen.add(Dense(2, activation='relu'))
+gen.summary()
+
+
+# In[ ]:
+
+
+gen.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+# In[ ]:
+
+
+fake = gen.predict(np.random.uniform(-1, 1, (2500, 200)))
+print(fake) 
+
+
+# In[ ]:
+
+
+x_training = np.ones((2500, 2))
+for i in range(2500):
+#     x_training[i][0] = np.random.randint(0, 23, (1, 1))
+#     x_training[i][1] = np.random.randint(0, 23, (1, 1))
+#     x_training[i][2] = np.random.randint(0, 59, (1, 1))
+#     x_training[i][3] = np.random.randint(0, 59, (1, 1))
+#     x_training[i][4] = np.random.randint(0, 59, (1, 1))
+#     x_training[i][5] = np.random.randint(0, 59, (1, 1))
+
+#     x_training[i][0] = np.random.randint(24, 100, (1, 1))
+#     x_training[i][1] = np.random.randint(24, 100, (1, 1))
+#     x_training[i][2] = np.random.randint(60, 100, (1, 1))
+#     x_training[i][3] = np.random.randint(60, 100, (1, 1))
+#     x_training[i][4] = np.random.randint(60, 100, (1, 1))
+#     x_training[i][5] = np.random.randint(60, 100, (1, 1))
+    
+    x_training[i][0] = np.random.randint(0, 24, (1, 1))
+    x_training[i][1] = np.random.randint(3600, 4800, (1, 1))
+print(x_training)
+x_training = np.concatenate((x_train, x_training))
+print(x_training)
+x_training = np.concatenate((x_training, fake))
+print(x_training)
+
+
+# In[ ]:
+
+
+x_training = np.concatenate((x_training, np.random.uniform(-1, 1, (5000, 2))))
+x_training = np.concatenate((x_training, np.zeros((1000, 2))))
+x_training
+
+
+# In[ ]:
+
+
+y_training = np.concatenate((y_train, np.zeros((11000, 1))))
+y_training
+
+
+# In[ ]:
+
+
+x_testing = np.concatenate((x_test, np.random.uniform(-1, 1, (2500, 2))), axis=0)
+x_testing
+
+
+# In[ ]:
+
+
+y_testing = np.concatenate((y_test, np.zeros((2500, 1))), axis=0)
+y_testing
+
+
+# In[ ]:
+
+
+GAN.add(gen)
+
+
+# In[ ]:
+
+
+dis.add(Dropout(0.4, input_shape=(2,)))
+dis.add(Dense(10, activation='relu'))
+# dis.add(LeakyReLU(alpha=0.2))
+dis.add(BatchNormalization(momentum=0.9))
+dis.add(Dense(5, activation='relu'))
+# dis.add(LeakyReLU(alpha=0.2))
+dis.add(BatchNormalization(momentum=0.9))
+dis.add(Dense(1, activation='sigmoid'))
+dis.summary(0)
+
+
+# In[ ]:
+
+
+print(x_training.shape)
+print(y_training.shape)
+
+
+# In[ ]:
+
+
+# import keras
+# from keras.callbacks import TensorBoard
+# cb = TensorBoard(log_dir='./Discriminate/DealWithIt', histogram_freq=0, write_graph=True, write_images=True)
+dis.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+dis.fit(x_training, y_training, batch_size=1000, epochs=10000, verbose=1, validation_data=(x_testing, y_testing))
+
+
+# In[ ]:
+
+
+# pred = dis.predict(x_predict)
+# print(pred)
+
+
+# In[ ]:
+
+
+dis.trainable = False
+
+
+# In[ ]:
+
+
+GAN.add(dis)
+
+
+# In[ ]:
+
+
+GAN.summary()
+GAN.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+# In[ ]:
+
+
+gen.predict(np.random.uniform(-1, 1, (1, 200)))
+
+
+# In[ ]:
+
+
+for i in range(1000):
+    noise = np.random.uniform(-1, 1, (32, 200))
+    ones = np.ones((32, 1))
+    print("Predicted Data:")
+    print(gen.predict(noise[0:1]))
+    print(dis.predict(gen.predict(noise[0:1])))
+    print("\n")
+    loss = GAN.train_on_batch(noise, ones)
+    print(loss)
+    print("Epoch: " + str(i))
+
+
+# In[ ]:
+
+
+random = np.random.uniform(-1, 1, (1, 200))
+print(random)
+data = gen.predict(random)
+print(data)
+pred = dis.predict(data)
+print(pred)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ Ben's Image GAN.py
@@ -0,0 +1,273 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from keras.datasets import mnist
+import numpy as np
+import pandas as pds
+import keras
+from keras.callbacks import TensorBoard
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.layers import Flatten
+from keras.layers import Reshape
+from keras.layers import UpSampling2D
+from keras.layers import UpSampling3D
+from keras.layers import BatchNormalization
+from keras.layers import Conv2DTranspose
+from keras.layers import Activation
+from keras.layers import Dropout
+from keras.layers import Conv2D
+from keras.layers.advanced_activations import LeakyReLU
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+from matplotlib import pyplot as plt
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', one_hot = True)
+def gen_image(arr):
+    two_d = (np.reshape(arr, (28, 28)) * 255).astype(np.uint8)
+    plt.imshow(two_d, interpolation='nearest')
+    return plt
+
+# Get a batch of two random images and show in a pop-up window.
+batch_xs, batch_ys = mnist.test.next_batch(2)
+def preImg(img, net):
+    gen_image(img).show()
+    print(net.predict(img))
+def img(img):
+    gen_image(img).show()
+
+
+# In[2]:
+
+
+y_training = np.ones(60000)
+y_testing = np.concatenate((np.ones((10000)), np.zeros(2000)), axis=0)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+dis = Sequential()
+gen = Sequential()
+GAN = Sequential()
+
+
+# In[4]:
+
+
+# gen.add(Dense(196, input_shape=(100,), activation='relu'))
+# gen.add(Reshape((14, 14,)))
+# gen.add(UpSampling2D(size=(2, 2)))
+# gen.summary()
+dim = 7
+depth = 256
+dropout = 0.6
+
+
+gen.add(Dense(784, input_dim=100))
+gen.add(BatchNormalization(momentum=0.9))
+gen.add(Activation('relu'))
+gen.add(Reshape((28, 28)))
+gen.add(Dropout(dropout))
+# In: dim x dim x depth
+# Out: 2*dim x 2*dim x depth/2
+#gen.add(UpSampling2D())
+gen.add(BatchNormalization(momentum=0.9))
+gen.add(Activation('relu'))
+gen.add(BatchNormalization(momentum=0.9))
+gen.add(Activation('relu'))
+gen.add(BatchNormalization(momentum=0.9))
+gen.add(Activation('relu'))
+#gen.add(Reshape((28, 28)))
+gen.summary()
+
+
+# In[5]:
+
+
+gen.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+# In[6]:
+
+
+fake = gen.predict(np.random.uniform(-1, 1, (10000, 100)))
+
+
+# In[7]:
+
+
+x_testing = np.random.uniform(0, 255, (2000, 28, 28))
+x_testing = np.concatenate((x_test, x_testing), axis=0)
+
+
+# In[8]:
+
+
+x_train[0].shape
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[9]:
+
+
+y_training = np.concatenate((y_training, np.zeros(60000)), axis=0)
+y_training.shape
+
+
+# In[10]:
+
+
+x_training = np.random.randint(0, 255, (50000, 28, 28))
+
+
+# In[11]:
+
+
+x_training = np.concatenate((x_train, x_training), axis=0)
+x_training = np.concatenate((x_training, fake), axis=0)
+x_training.shape
+
+
+# In[12]:
+
+
+dis.add(Dropout(0.6, input_shape=(28, 28,)))
+dis.add(Flatten(input_shape=(28, 28,)))
+dis.add(Dense((784)))
+dis.add(LeakyReLU(alpha=0.2))
+dis.add(Dense(392))
+dis.add(LeakyReLU(alpha=0.2))
+dis.add(Dense(1, activation='sigmoid'))
+dis.summary()
+
+
+# In[13]:
+
+
+dis.compile(metrics=['accuracy'], loss='binary_crossentropy', optimizer='adam')
+dis.fit(x_training, y_training, batch_size=250, epochs=20, verbose=1, validation_data=(x_testing, y_testing))
+
+
+# In[14]:
+
+
+test = np.random.uniform(0, 255, (1, 28, 28))
+preImg(test, dis)
+i = 0
+j = 0
+while i < 10:
+    if y_train[j] == i:
+        preImg(x_train[j:j+1], dis)
+        i += 1
+    j += 1
+
+
+# In[ ]:
+
+
+
+
+
+# In[15]:
+
+
+GAN.add(gen)
+
+
+# In[16]:
+
+
+dis.trainable = False
+GAN.add(dis)
+
+
+# In[17]:
+
+
+GAN.summary()
+GAN.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+pred = gen.predict(np.random.uniform(-1, 1, (1, 100)))
+gen_image(pred).show()
+
+
+# In[19]:
+
+
+for i in range(10000):
+    noise = np.random.uniform(-1, 1, (15, 100))
+    ones = np.ones((15, 1))
+    preImg(gen.predict(noise[0:1]), dis)
+    loss = GAN.train_on_batch(noise, ones)
+    print(loss)
+    print("Epoch: " + str(i))
+# GAN.fit(noise, ones, epochs=50000, batch_size=100, verbose=1, validation_split=0.2)
+
+
+# In[ ]:
+
+
+GAN.predict(np.random.uniform(-1, 1, (1, 100)))
+
+
+# In[ ]:
+
+
+pred = gen.predict(np.random.uniform(-1, 1, (1, 100)))
+preImg(pred, dis)
+print(pred)
+
+
+# In[ ]:
+
+
+dis.predict(np.random.uniform(0, 255, (1, 28, 28)))
+
+
+# In[ ]:
+
+
+img = gen.predict(np.random.uniform(-1, 1, (1, 100)))
+
+
+pred = gen.predict(np.random.uniform(-1, 1, (1, 100)))
+preImg(pred, dis)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
